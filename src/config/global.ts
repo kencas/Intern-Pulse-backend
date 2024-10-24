154028e1522a25c854e6bf9e4348df25acb3ed17
kencas <kcjojo8@gmail.com>
2024-10-24 13:24:39 +0100

    Fix

--- src/config/global.ts
+++ src/config/global.ts
@@ -3,7 +3,7 @@ export const env = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	NODE_PORT: process.env.PORT || 3000,
 	DB_USERNAME: process.env.DB_USERNAME || 'postgres',
-	DB_PASSWORD: process.env.DB_PASSWORD || '',
+	DB_PASSWORD: process.env.DB_PASSWORD || 'kencas@45',
 	DB_PORT: process.env.DB_PORT || '5432',
 	DB_HOST: process.env.DB_HOST || 'localhost',
 	DB_NAME: process.env.DB_NAME || 'intern-pulse',
